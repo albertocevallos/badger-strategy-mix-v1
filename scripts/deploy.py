c6efdef073f56e0337c975e5fe03d755ae07bd6f
Alberto Cevallos <acvllsh@gmail.com>
2021-08-15 22:54:41 -0400

    deploy key config tool

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -81,14 +81,14 @@ def deploy():
   controller.setStrategy(WANT, strategy, {"from": deployer})
 
   ## Uniswap some tokens here
-  router = Contract.from_explorer("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-  router.swapExactETHForTokens(
-    0, ## Mint out
-    ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", WANT],
-    deployer,
-    9999999999999999,
-    {"from": deployer, "value": 5000000000000000000}
-  )
+  # router = Contract.from_explorer("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
+  # router.swapExactETHForTokens(
+  #   0, ## Mint out
+  #   ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", WANT],
+  #   deployer,
+  #   9999999999999999,
+  #   {"from": deployer, "value": 5000000000000000000}
+  # )
 
   return DotMap(
     deployer=deployer,
